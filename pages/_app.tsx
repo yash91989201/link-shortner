f8e3c5a6a45bdb2a97b45d8e03c9d6c93b569203
yash91989201 <yashraj6370@gmail.com>
2022-10-11 23:57:33 +0530

    add next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
+import type { Session } from "next-auth"
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface Props extends AppProps {
+  pageProps: {
+    session: Session
+  }
+}
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: Props) {
+  return <SessionProvider session={session}>
+    <Component {...pageProps} />
+  </SessionProvider>
 }
 
 export default MyApp
--- pages/_middleware.tsx
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse, NextFetchEvent } from "next/server";
-
-export default async function middleware(req: NextRequest, res: NextResponse, event: NextFetchEvent) {
-    if (req.nextUrl.pathname.startsWith("/api/get-url")) {
-        console.log("returning early");
-        return
-    }
-
-}
