f1ab2b3896f2e665d448f7232b4bb4e58e99b609
yash91989201 <yashraj6370@gmail.com>
2022-11-17 18:45:13 +0530

    update api url to include env vars

--- utils/link.ts
+++ utils/link.ts
@@ -3,10 +3,13 @@ import type { LinkStats, ShortLink } from "@prisma/client";
 async function getLinks(
   userId: GetLinkVars
 ): Promise<GetLinkResult<ShortLink[]>> {
-  const fetchResult = await fetch("http://localhost:3000/api/link", {
-    method: "POST",
-    body: JSON.stringify({ userId }) as string,
-  });
+  const fetchResult = await fetch(
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/link`,
+    {
+      method: "POST",
+      body: JSON.stringify({ userId }) as string,
+    }
+  );
   const res = await fetchResult.json();
 
   return res;
@@ -18,7 +21,7 @@ async function createLink({
   slug,
 }: CreateLinkVars): Promise<CreateLinkResult<ShortLink[]>> {
   const fetchResult = await fetch(
-    "http://localhost:3000/api/link/create-link",
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/link/create-link`,
     {
       method: "POST",
       headers: {
@@ -37,7 +40,7 @@ async function createLink({
 
 async function deleteLink({ id }: DeleteLinkVars) {
   const fetchResult = await fetch(
-    "http://localhost:3000/api/link/delete-link",
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/link/delete-link`,
     {
       method: "POST",
       headers: {
@@ -57,17 +60,20 @@ async function getLinkStats({
   fromDate,
   toDate,
 }: GetLinkStatsVars): Promise<GetLinkStatsResult<LinkStats[]>> {
-  const fetchResult = await fetch("http://localhost:3000/api/link/stats", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      slug,
-      fromDate,
-      toDate,
-    }),
-  });
+  const fetchResult = await fetch(
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/link/stats`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        slug,
+        fromDate,
+        toDate,
+      }),
+    }
+  );
   const res = await fetchResult.json();
   return res;
 }
