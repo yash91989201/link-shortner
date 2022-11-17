1ddbc872ecbe1a52ae7967f88ee93fe3a38e85be
Yashraj Jaiswal <68579045+yash91989201@users.noreply.github.com>
2022-11-17 18:16:05 +0530

    change page name to nextauth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "db/prisma-client";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/signin",
+  },
+});
